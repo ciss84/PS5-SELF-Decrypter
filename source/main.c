99da1602a479719852829c0df968aff523031d3d
Mugi <37186888+ciss84@users.noreply.github.com>
2025-02-15 14:47:11 +0100

    test

--- source/main.c
+++ source/main.c
@@ -945,8 +945,8 @@ int main()
     // i did this so when i pass in `/mnt/sandbox/pfsmnt` it will only dump `/mnt/sandbox/pfsmnt/PPSA01487-app0-patch0-union`
     // bc for ps5 games, `app0` and `app0-patch0-union` has the same files
 
-    dump_queue_add_file(sock, "/system/common/lib/libkernel_sys.sprx");
-    //dump_queue_add_dir(sock, "/mnt/sandbox/pfsmnt", 1);
+    //dump_queue_add_file(sock, "/system/common/lib/libkernel_sys.sprx");
+    dump_queue_add_dir(sock, "/mnt/sandbox/pfsmnt", 1);
     dump(sock, authmgr_handle, &offsets, "/data/dump");
 
 out:
